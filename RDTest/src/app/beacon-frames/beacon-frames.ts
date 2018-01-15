49970b546510c519b49545037c53419b7d3b27c7
Sathvik777 <katam.sathvik@gmail.com>
2018-01-15 17:03:00 +0100

    Changes to replicate exact response

--- RDTest/src/app/beacon-frames/beacon-frames.ts
+++ RDTest/src/app/beacon-frames/beacon-frames.ts
@@ -1,4 +1,23 @@
-export class BeaconFrame {
-    box: string;
-    rssi: number;
+export interface BeaconFrame {
+    Beacon: string;
+    Box: string;
+    TxPower: number;
+    Count: number;
+    Rssi: number;
+    RssiVar: number;
+    RssiMean: number;
+    Channel: number;
+    Radio: string;
+    Type: string;
+    Timestamp: string;
+    ServerTimestamp: string;
+    ClientTimestamp: string;
+    Data: string;
+    Range: number;
+    Toa: number;
+    Seq: number;
+    Flags: number;
+    Latitude: number;
+    Longitude: number;
   }
+  
--- RDTest/src/app/mock-beacon-frames.ts
+++ RDTest/src/app/mock-beacon-frames.ts
@@ -2,20 +2,48 @@ import { BeaconFrame } from './beacon-frames/beacon-frames';
 
 export const BeaconFrames : BeaconFrame[] = [
     {
-        box : "78c40e123456", rssi :  -70
-    }, 
-    {
-        box : "78c40e123457", rssi :  -74
+        Beacon: "12000000000256d9",
+        Box: "78c40e104c45",
+        TxPower: -59,
+        Count: 1,
+        Rssi: -78,
+        RssiVar: 0,
+        RssiMean: -70,
+        Channel: 39,
+        Radio: "BLE",
+        Type: "iBeacon",
+        Timestamp: "2018-01-15 15:35:42.294",
+        ServerTimestamp: "2018-01-15 15:35:43.178",
+        ClientTimestamp: "2018-01-15 15:35:45.196",
+        Data: "",
+        Range: 0,
+        Toa: 0,
+        Seq: 255,
+        Flags: 0,
+        Latitude: 0,
+        Longitude: 0
     },
     {
-        box : "78c40e123458", rssi :  -73
-    },
-    {
-        box : "78c40e123459", rssi :  -65
-    },  
-    {
-        box : "78c40e123460", rssi :  -80
+        Beacon: "12000000000256d9",
+        Box: "78c40e104c45",
+        TxPower: -59,
+        Count: 1,
+        Rssi: -78,
+        RssiVar: 0,
+        RssiMean: -70,
+        Channel: 39,
+        Radio: "BLE",
+        Type: "iBeacon",
+        Timestamp: "2018-01-15 15:35:42.294",
+        ServerTimestamp: "2018-01-15 15:35:43.178",
+        ClientTimestamp: "2018-01-15 15:35:45.196",
+        Data: "",
+        Range: 0,
+        Toa: 0,
+        Seq: 255,
+        Flags: 0,
+        Latitude: 0,
+        Longitude: 0
     }
-
 ];
 
